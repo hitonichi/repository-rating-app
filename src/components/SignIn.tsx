d72b63e81f43da46780b600dcb22d292f5246367
Tran Nhat Duy <42115291+hitonichi@users.noreply.github.com>
2023-09-07 01:35:33 +0700

    feat: fine-tune typing

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,9 +1,19 @@
 import { Formik } from "formik";
 import SignInForm from "./SignInForm";
 
+interface IFormValue {
+  username: string;
+  password: string;
+}
+
 const SignIn = () => {
+  const initialFormValue: IFormValue = { username: "", password: "" };
+
   return (
-    <Formik initialValues={{}} onSubmit={() => console.log("submitted")}>
+    <Formik
+      initialValues={initialFormValue}
+      onSubmit={(val: IFormValue) => console.log("submitted", val, typeof val)}
+    >
       {({ handleSubmit }) => <SignInForm onSubmit={handleSubmit} />}
     </Formik>
   );
